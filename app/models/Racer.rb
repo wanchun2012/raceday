5e790cf69ed8b5763132e28d9a614602ef44abae
wanchun2012 <wanchun0616@gmail.com>
2016-10-31 07:13:34 +0000

    Update CRUD

--- app/models/Racer.rb
+++ app/models/Racer.rb
@@ -3,19 +3,15 @@ class Racer
 
 	attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
 
-	def to_s
-		"#{@id}: #{@number}: #{@first_name}: #{@last_name}: #{@gender}: #{@group}: #{@secs}"
-	end
-
 	# initialize from both a Mongo and Web hash
 	def initialize(params={})
-		@id = params[:_id].nil? ? paramsp[:id] : params[:_id]
-		@number = params[:number]
+		@id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+		@number = params[:number].to_i
 		@first_name = params[:first_name]
 		@last_name = params[:last_name]
 		@gender = params[:gender]
 		@group = params[:group]
-		@secs = params[:secs]
+		@secs = params[:secs].to_i
 	end
 
 	# tell Rails whether this instance is persisted
@@ -23,13 +19,13 @@ class Racer
     	!@id.nil?
   	end
   	
-  	def created_at
-    	nil
-  	end
+	def created_at 
+		nil
+	end
 
-  	def updated_at
-    	nil
-  	end
+	def updated_at
+		nil
+	end
 
 	# convenience method for access to client in console
 	def self.mongo_client
@@ -52,7 +48,65 @@ class Racer
 	# limit the number of documents returned if limit is specified
 	# return the result
 	def self.all(prototype={}, sort={:number=>1}, skip=0, limit=nil)
-		collecton.find.projection({_id:true, number:true, first_name:true, last_name:true, gender:true, group:true, secs: true})
+		Rails.logger.debug {'getting all racers, prototype=#{prototype}, sort=#{sort}, offset=#{skip}, limit=#{limit}'}
+
+		result = collection.find(prototype)
+			.projection({_id:true, number:true, first_name:true, last_name:true, gender:true, group:true, secs: true})
+			.sort(sort)
+			.skip(skip)
+		result = result.limit(limit) if !limit.nil?
+		return result
 	end
+
+	# accept a single id parameter that is either a string or BSON::ObjectId Note: it must be able to handle either format.
+	# find the specific document with that _id
+	# return the racer document represented by that id
+	def self.find id
+		Rails.logger.debug {"getting racer #{id}"}
+		result = collection.find({'_id': BSON::ObjectId.from_string(id)}).first
+		return result.nil? ? nil : Racer.new(result)
+	end
+
+	# take no arguments
+	# insert the current state of the Racer instance into the database
+	# obtain the inserted document _id from the result 
+	# and assign the to_s value of the _id to the instance attribute @id
+	def save
+		Rails.logger.debug {"saving #{self}"}
+
+		result = self.class.collection
+					.insert_one(number:@number, first_name:@first_name, last_name:@last_name, gender:@gender, group:@group, secs: @secs)
+		@id = result.inserted_id.to_s
+	end
+
+	# accept a hash as an input parameter
+	# updates the state of the instance variables – except for @id. 
+	# That never should change. • find the racer associated with the current @id instance variable in the database
+	# update the racer with the supplied values – replacing all values
+def update(params) 
+	Rails.logger.debug {"updating #{self} with #{params}"}
+	
+    @number = params[:number].to_i
+    @first_name = params[:first_name] 
+    @last_name = params[:last_name]  
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs)
+    self.class.collection.find(:_id => BSON::ObjectId.from_string(@id))
+                         .update_one(params)
+  end
+
+	# accept no arguments
+	# find the racer associated with the current @number instance variable in the database 
+	# remove that instance from the database
+	def destroy
+    	Rails.logger.debug {"destroying #{self}"}
+
+    	self.class.collection
+              .find(:_id => BSON::ObjectId.from_string(@id))
+              .delete_one   
+  	end  
 end
 	
